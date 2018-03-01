6e424d976f718c6cf63dec246eb54e4a2ce0b3db
thangtqvn <quocthang.2001@gmail.com>
2018-03-01 12:37:31 +0700

    Upload files

--- wp-content/plugins/clever-swatches/assets/js/single-product-page-swatch.js
+++ wp-content/plugins/clever-swatches/assets/js/single-product-page-swatch.js
@@ -172,7 +172,7 @@
             });
 
             //click function for image/text/color types option
-            $(document).on('click', '.variations_form .zoo-cw-attribute-option', function () {
+            $(document).on('mousedown touched', '.variations_form .zoo-cw-attribute-option', function () {
                 var form_add_to_cart = $(this).parents('.variations_form');
                 var attribute_rows = form_add_to_cart.find('.zoo-cw-group-attribute');
                 var current_attribute_row = $(this).parents('.zoo-cw-group-attribute');
