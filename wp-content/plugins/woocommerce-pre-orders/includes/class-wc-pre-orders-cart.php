0ae1f6e4bb916571fe5f979748e181a5790746c4
thangtqvn <quocthang.2001@gmail.com>
2018-03-20 08:40:32 +0700

    Upload files

--- wp-content/plugins/woocommerce-pre-orders/includes/class-wc-pre-orders-cart.php
+++ wp-content/plugins/woocommerce-pre-orders/includes/class-wc-pre-orders-cart.php
@@ -159,8 +159,12 @@ class WC_Pre_Orders_Cart
 	 */
 	public function validate_cart ( $valid, $product_id )
 	{
-		//@TODO return because we can add both type of product to cart
-		return $valid;
+		$pre_order_charge = get_post_meta( $post_id, '_wc_pre_orders_when_to_charge', true );
+		if ($pre_order_charge == 'upfront')
+		{
+			//@TODO return because we can add both type of product to cart
+			return $valid;
+		}
 		
 		
 		global $woocommerce;
