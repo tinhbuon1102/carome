a6b6722178bb489be3017035e18cf4a2af5fc875
Tinhbuon1102 <quocthang.2001@gmail.com>
2019-02-14 00:46:25 +0900

    Server Upload files

--- wp-content/themes/elsey-child/functions.php
+++ wp-content/themes/elsey-child/functions.php
@@ -1,4 +1,8 @@
 <?php
+if (isset($_GET['test']))
+{
+	include dirname(__FILE__) . '/functions_test.php';
+}
 define('CONTACT_EMAIL_ADMIN_WITH_FILE', 'return@carome.net');
 
 if (strpos($_SERVER['SERVER_NAME'], 'carome.net') !== false){
@@ -78,92 +82,7 @@ function shortcode_url() {
 }
 
 function remove_product_editor() {
-
 	remove_post_type_support( 'product', 'editor' );
-	if (isset($_GET['test_epsilon']))
-	{
-		$epsilon_response = get_post_meta($_GET['test_epsilon'], 'epsilon_response_array', true);
-		$epsilon_data = array();
-		foreach ( $epsilon_response['result'] as $uns_k => $uns_v )
-		{
-			list ($result_atr_key, $result_atr_val) = each($uns_v);
-			$epsilon_data[$result_atr_key] = $result_atr_val;
-		}
-		
-		$order_id = mb_ereg_replace('[^0-9]', '', $epsilon_data['order_number']);
-		$order = wc_get_order($order_id);
-
-		$content_folder = dirname(dirname(dirname(__FILE__)));
-		include_once ($content_folder . '/plugins/wc4jp-epsilon/includes/gateways/epsilon/includes/class-wc-gateway-epsilon-request.php');
-		require_once $content_folder . "/plugins/wc4jp-epsilon/includes/gateways/epsilon/includes/http/Request.php";
-		require_once $content_folder . "/plugins/wc4jp-epsilon/includes/gateways/epsilon/includes/xml/Unserializer.php";
-
-		$epsilon_request = new WC_Gateway_Epsilon_Request('WC_Gateway_Epsilon_Pro_CS');
-		$gateway_id = 'epsilon_pro_cs';
-		// $response_array = $epsilon_request->get_order_to_epsilon( $order ,$epsilon_data['contract_code'], $epsilon_data['st_code'], true, $gateway_id,
-		// $epsilon_data['conveni_code'] );
-
-		// http_requset option Setting
-		$option = array(
-			"timeout" => "20" // Seconds
-			                  // "allowRedirects" => true, // redirect Stting(true/false)
-			                  // "maxRedirects" => 3, // max times of redirect
-		);
-		$epsilon_sc_settings = get_option('woocommerce_epsilon_pro_cs_settings'); 
-		pr($epsilon_data);
-die;
-		// HTTP_Request Initialization
-		$request = new HTTP_Request($epsilon_request->get_request_url($epsilon_sc_settings['testmode']), $option);
-
-		// set method
-		$request->setMethod(HTTP_REQUEST_METHOD_POST);
-		// set post data
-		$request->addPostData('memo2', 'woocommerce');
-		$request->addPostData('xml', '1');
-		$request->addPostData('currency_id', get_woocommerce_currency());
-		if ( $gateway_id == 'epsilon_pro_cs' )
-		{
-			foreach ($epsilon_data as $data_key => $data_value)
-			{
-				$request->addPostData($data_key, $data_value);
-			}
-			$request->addPostData('user_mail_add', $order->billing_email);
-			
-// 			$request->addPostData('conveni_code', $epsilon_data['conveni_code']);
-// 			$request->addPostData('contract_code', $epsilon_data['contract_code']);
-// 			$request->addPostData('trans_code', $epsilon_data['trans_code']);
-// 			$request->addPostData('receipt_no', $epsilon_data['receipt_no']);
-// 			$request->addPostData('kigyou_code', $epsilon_data['kigyou_code']);
-// 			$request->addPostData('haraikomi_url', $epsilon_data['haraikomi_url']);
-// 			$request->addPostData('paid', $epsilon_data['paid']);
-// 			$request->addPostData('receipt_date', $epsilon_data['receipt_date']);
-// 			$request->addPostData('conveni_limit', $epsilon_data['conveni_limit']);
-// 			$request->addPostData('conveni_time', $epsilon_data['conveni_time']);
-		}
-
-		// HTTP REQUEST Action
-		$response = $request->sendRequest();
-		if ( ! PEAR::isError($response) )
-		{
-			$res_code = $request->getResponseCode();
-			$res_content = $request->getResponseBody();
-			pr($request);
-			var_dump($res_content);
-			//xml unserializer
-			$temp_xml_res = str_replace("x-sjis-cp932", "UTF8", $res_content);
-			$unserializer = new XML_Unserializer();
-			$unserializer->setOption('parseAttributes', TRUE);
-			$unseriliz_st = $unserializer->unserialize($temp_xml_res);
-			
-			if ($unseriliz_st === true) {
-				$res_array = $unserializer->getUnserializedData();
-			}
-			pr($res_array);
-			pr('---------------------------------');
-			die();
-		}
-	}
-
 }
 add_action( 'init', 'remove_product_editor' );
 
