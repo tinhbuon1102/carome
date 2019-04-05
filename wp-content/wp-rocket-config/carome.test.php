753dd2a49df490c4211308350ea39ecf8cd8923c
thangtqvn <thangtqvn@MacBook-Pro-cua-thangtqvn.local>
2019-04-05 11:15:26 +0700

    Upload files

---
+++ wp-content/wp-rocket-config/carome.test.php
@@ -0,0 +1,17 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+$rocket_cookie_hash = '81b04bf4f0f763c91dea303db4ef1f55';
+$rocket_cache_mobile_files_tablet = 'desktop';
+$rocket_cache_mobile = '1';
+$rocket_cache_ssl = '1';
+$rocket_cache_reject_uri = '/shop/(.*)|/(.+/)?feed/?|/checkout/(.*)|/cart/|/my-account/(.*)|/wc-api/v(.*)|/(index\.php/)?wp\-json(/.*|$)';
+$rocket_cache_reject_cookies = 'MobileSafari|Safari|wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_ua = 'facebookexternalhit';
+$rocket_cache_query_strings = array (
+);
+$rocket_secret_cache_key = '5c0fe7c27c6a3476867704';
+$rocket_do_caching_mobile_files = '0';
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array (
+);
