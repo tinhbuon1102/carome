023cab5edc60a8e8c5c0205ec446b8e63ba4ba4d
thangtqvn <thangtqvn@MacBook-Pro-cua-thangtqvn.local>
2019-06-05 13:57:01 +0700

    Upload files

--- wp-content/themes/elsey-child/functions.php
+++ wp-content/themes/elsey-child/functions.php
@@ -2161,7 +2161,7 @@ function elsey_wp_ajax_load_member_age_by_order()
 			</div>';
 			
 		}
-		$response['counting'] = array('allCount' => $allRangeCount, 'rangeCount' => $allRangeCount);
+		$response['counting'] = array('allCount' => $allCount, 'rangeCount' => $allRangeCount);
 		$response['date'] = sprintf(__('The age list calculated at %1$s', 'elsey'), current_time('mysql'));
 		update_option('dashboard_age_member_html', $response['content']);
 		update_option('dashboard_age_member_date', current_time('mysql'));
