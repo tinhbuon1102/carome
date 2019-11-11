99e2e9fc1d940fc3049cc87abbc6441915a38d10
thangtqvn <thangtqvn@B-Phone-M.local>
2019-11-11 11:51:46 +0700

    Upload files

--- wp-content/themes/elsey-child/functions_epsilon.php
+++ wp-content/themes/elsey-child/functions_epsilon.php
@@ -24,7 +24,7 @@ function check_epsilon_paid_cs_orders ()
 		ORDER BY wp_posts.menu_order ASC, wp_posts.post_date DESC LIMIT 0, 222";
 	
 	$orders = $wpdb->get_results($sql);
-	echo '<pre>'; print_r($orders); echo '</pre>';die;
+	echo '<pre>'; print_r($orders); echo '</pre>';die; 
 	
 	if (is_array($orders) && !empty($orders))
 	{
