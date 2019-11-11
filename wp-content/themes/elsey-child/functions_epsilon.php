35d81d04bc9c1a56c71e5e49247481d1ee45db45
thangtqvn <thangtqvn@B-Phone-M.local>
2019-11-11 11:57:15 +0700

    Upload files

--- wp-content/themes/elsey-child/functions_epsilon.php
+++ wp-content/themes/elsey-child/functions_epsilon.php
@@ -20,10 +20,12 @@ function check_epsilon_paid_cs_orders ()
 		  AND
 		  ( (mt1.meta_key = '_custom_payment_status' AND mt1.meta_value != '1' AND (wp_posts.post_status = 'wc-on-hold' OR wp_posts.post_status = 'wc-processing' OR wp_posts.post_status = 'wc-pending'))
 		)) AND wp_posts.post_type = 'shop_order'  
+		AND wp_posts.post_date > '" . date('Y-m-d', strtotime('-5 days')) . "' 
 		GROUP BY wp_posts.ID
 		ORDER BY wp_posts.menu_order ASC, wp_posts.post_date DESC LIMIT 0, 20";
 	
 	$orders = $wpdb->get_results($sql);
+	echo '<pre>'; print_r($orders); echo '</pre>';die;
 	
 	if (is_array($orders) && !empty($orders))
 	{
